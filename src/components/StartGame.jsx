7d20c9630a05c2c55a988d28bb90a21c7cd48f24
rkpust <rezaul.cse.pust17@gmail.com>
2023-12-21 12:09:30 +0600

    added styled Button component

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Button } from "./styled/Button";
 
 const StartGame = ({toggle}) => {
   return (
@@ -31,29 +32,4 @@ const Container = styled.div`
         margin-bottom: 5px;
       }
     }
-`;
-
-const Button = styled.button`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    color: white;
-    padding: 10px 18px;
-    min-width: 220px;
-    background: #000000;
-    border-radius: 5px;
-    font-size: 16px;
-    border: 1px solid transparent;
-    cursor: pointer;
-    transition: 0.4s background ease-in;
-
-    &:hover {
-      background-color: white;
-      border: 1px solid black;
-      color: black;
-      transition: 0.3s background ease-in;
-    }
-
 `;
